391487382d2027e1a2b4cdbf6104913d11e38917
Nishit Ranjan <nranjan.2004@gmail.com>
2026-09-02 14:20:40 +0530

    graph class and dyn programming code added

---
+++ algorithms/dynamicPrograaming/climbStairs.js
@@ -0,0 +1,16 @@
+function climbStairs(n){
+
+    if(n===1) return 1
+    if(n===2) return 2
+
+    const dp = new Array(n+1)
+
+    dp[1] = 1
+    dp[2] = 2
+
+    for(let i=3; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+
+    return dp[n]
+}
--- dataStructures/Graph.js
+++ dataStructures/Graph.js
@@ -1,10 +1,39 @@
 class Graph{
     constructor(){
-        this.adjacencyList = {}
+        this.adjacencyList = new Map()
     }
 
     addVertex(vertex){
 
-        
+        if(!this.adjacencyList.has(vertex)){
+            this.adjacencyList.set(vertex, [])
+        }
     }
+
+    addEdge(vertex1, vertex2){
+        this.adjacencyList.get(vertex1).push(vertex2)
+        this.adjacencyList.get(vertex2).push(vertex1)
+    }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList.set(
+        vertex1,
+        this.adjacencyList.get(vertex1)
+            .filter(vertex => vertex !== vertex2)
+    );
+
+    this.adjacencyList.set(
+        vertex2,
+        this.adjacencyList.get(vertex2)
+            .filter(vertex => vertex !== vertex1)
+    );
+}
+removeVertex(vertex){
+    const neighbors = this.adjacencyList.get(vertex)
+
+    for(let neighbor of neighbors){
+        this.removeEdge(vertex, neighbor)
+    }
+    this.adjacencyList.delete(vertex)
+}
 }
